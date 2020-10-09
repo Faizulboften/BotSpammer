b14d584f55c0056c8fe377822feadce6a516cd42
Faizulboften <72049391+Faizulboften@users.noreply.github.com>
2020-10-09 09:43:43 +0800

    Create Spammer.py

---
+++ Spammer.py
@@ -0,0 +1,12 @@
+import os,sys,time,requests,re,json,random
+from random import randrange as rg
+print ("\033[00m")
+def clear():
+    os.system("clear")
+def balik():
+    f=input("\t[enter to back]")
+    if f == "":
+       os.system("python spam.py")
+    else:
+       sys.exit()
+def baner():
